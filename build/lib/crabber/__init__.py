53e2c0c05f1c6cff9b2d404bcc1b681b160e9160
Jake Ledoux <contactjakeledoux@gmail.com>
2021-01-26 16:47:22 -0500

    Python module setup

---
+++ build/lib/crabber/__init__.py
@@ -0,0 +1,3 @@
+""" Python-Crabber client for the Crabber.net REST API.
+"""
+from .models import API, Bio, Crab, Molt
---
+++ build/lib/crabber/exceptions.py
@@ -0,0 +1,10 @@
+class MaxTriesError(BaseException):
+    """ Raised when attempts to make a request exceed maximum number of attempts.
+    """
+    pass
+
+
+class RequiresAuthenticationError(BaseException):
+    """ Raised when attempting to make a request without proper authentication.
+    """
+    pass
