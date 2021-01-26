5c1c36a0324b1488900ee4320166612d628258d1
Jake Ledoux <contactjakeledoux@gmail.com>
2021-01-26 17:34:46 -0500

    Fixed twine errors

--- setup.py
+++ setup.py
@@ -3,17 +3,15 @@ from setuptools import setup, find_packages
 with open('README.md') as f:
     readme = f.read()
 
-with open('LICENSE') as f:
-    license = f.read()
-
 setup(
     name='python-crabber',
     version='0.1.0',
     description='A Python client for the Crabber REST API.',
     long_description=readme,
+    long_description_content_type='text/markdown',
     author='Jake Ledoux',
     author_email='contactjakeledoux@gmail.com',
     url='https://github.com/jakeledoux/python-crabber',
-    license=license,
+    license='GNU General Public License v2.0',
     packages=find_packages(exclude=('tests', 'docs'))
 )
