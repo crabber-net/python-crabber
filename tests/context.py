6cb5da705297af8b314da61a03cd182af672c37e
Jake Ledoux <contactjakeledoux@gmail.com>
2021-01-26 16:59:23 -0500

    Test API key

--- tests/context.py
+++ tests/context.py
@@ -3,4 +3,4 @@ import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 import crabber
-API_KEY = os.environ.get('CRABBER_API_KEY')
+API_KEY = '8bfe75a1d4b7d31ec78c7939fe8744f3'
